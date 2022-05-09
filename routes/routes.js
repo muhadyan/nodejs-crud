05d6c8d2a3e1a81638a301403bdd14a38d2f1f73
M Adyan Rohutomo <muhadyan@192.168.104.13>
2022-05-09 16:29:33 +0700

    feat/read excel and input to db

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer = require("multer");
+const xlsx = require("xlsx");
 const handleBook = require("../handler/book");
 const router = express.Router();
 
@@ -7,5 +9,30 @@ router.get("/api/book/:no", handleBook.handleGetBookByNo);
 router.post("/api/book", handleBook.handlePostBooks);
 router.patch("/api/book/:no", handleBook.handleUpdateBook);
 router.delete("/api/book/:no", handleBook.handleDeleteBook);
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+router.post("/api/books-excel", upload.single("excel"), (req, res) => {
+  const buf = req.file.buffer;
+  const workbook = xlsx.read(buf).Sheets.Sheet1;
+
+  for (let i = 2; i > 0; i++) {
+    A = `A${i}`;
+    B = `B${i}`;
+    C = `C${i}`;
+    const no = workbook[A];
+    const book = workbook[B];
+    const author = workbook[C];
+    if (no === undefined) {
+      break;
+    } else {
+      console.log(no.v);
+      console.log(book.v);
+      console.log(author.v);
+    }
+  }
+
+  res.send("success");
+});
 
 module.exports = router;
